74b5aecc7fbfff23e665a92b24393157b653c99a
cgross <clement@sbgsportssoftware.com>
2018-05-16 16:20:05 +0100

    wip auth firebase

--- app/src/main/java/citroentrex/mashfestival/MainActivity.java
+++ app/src/main/java/citroentrex/mashfestival/MainActivity.java
@@ -1,24 +1,66 @@
 package citroentrex.mashfestival;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth_;
+    private static final String TAG = "AnonymousAuth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth_ = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        FirebaseUser current_user = auth_.getCurrentUser();
+        signInAnonymously();
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+    }
+
+    private void signInAnonymously() {
+       // showProgressDialog();
+        // [START signin_anonymously]
+        auth_.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = auth_.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                           /* Toast.makeText(AnonymousAuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);*/
+                        }
+
+                        // [START_EXCLUDE]
+                       // hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END signin_anonymously]
     }
 }
